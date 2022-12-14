35d902e9c6aab4384875734af3fc5b5d3fcc07ea
sagarika28 <sagarika28r@gmail.com>
2022-12-14 14:38:39 +0000

    submission one

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args)
+  { 
+ RWOnly obj = new RWOnly();
+//  System.out.print(obj.name); // build error, cannot access private member 
+      
+    obj.setter("saga");
+    System.out.print(obj.getter());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name = "Sagarika";
+    void setter(String name){
+        this.name = name;
+    }
+    String getter()
+    {
+        return name;
+    }
 }
